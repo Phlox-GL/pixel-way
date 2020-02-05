a11fabcc1524aa381d7b1959f79306568e1a0050
jiyinyiyong <jiyinyiyong@gmail.com>
2020-02-06 00:15:33 +0800

    small optimizations to list and bgs

--- src/app/container.cljs
+++ src/app/container.cljs
@@ -35,28 +35,34 @@
              (->> xs
                   (map-indexed
                    (fn [xi v]
-                     [(str xi "+" yi)
-                      (rect
-                       {:options {:x (* xi 15), :y (* yi 15), :width 14, :height 14},
-                        :fill (case v
-                          1 (hslx 120 80 80)
-                          true (hslx 200 80 40)
-                          false (hslx 200 60 20)
-                          (hslx 0 0 70)),
-                        :on {:mouseover (if v
-                               (fn [e d!] (d! :turn {:x xi, :y yi}))
-                               (fn [e d!] ))}})])))))
-          (apply concat)))
-    (rect
-     {:options {:x 800, :y 40, :width 60, :height 34},
-      :fill (hslx 40 80 80),
-      :on {:pointerdown (fn [e d!] (on-reset d!))}}
-     (text
-      {:text "Reset!",
-       :position {:x 808, :y 44},
-       :style {:fill (hslx 120 80 20), :font-size 18, :font-family "Josefin Sans"}}))
-    (if (:win? store)
+                     (rect
+                      {:options {:x (* xi 15), :y (* yi 15), :width 14, :height 14},
+                       :fill (case v
+                         1 (hslx 120 80 80)
+                         true (hslx 200 80 40)
+                         false (hslx 200 60 20)
+                         (hslx 0 0 70)),
+                       :on {:mouseover (if v
+                              (fn [e d!] (d! :turn {:x xi, :y yi}))
+                              (fn [e d!] ))}}))))))
+          (apply concat)
+          (map-indexed (fn [idx x] [idx x]))))
+    (container
+     {:position {:x 800, :y 40}}
+     (rect
+      {:options {:x 0, :y 0, :width 60, :height 34},
+       :fill (hslx 40 80 80),
+       :on {:pointerdown (fn [e d!] (on-reset d!))}}
       (text
-       {:text "Winning!",
-        :position {:x 140, :y 100},
-        :style {:fill (hslx 30 90 100), :font-size 200, :font-family "Josefin Sans"}})))))
+       {:text "Reset!",
+        :position {:x 8, :y 4},
+        :style {:fill (hslx 120 80 20), :font-size 18, :font-family "Josefin Sans"}})))
+    (if (:win? store)
+      (container
+       {:position {:x 140, :y 100}}
+       (rect
+        {:options {:x 0, :y 0, :width 800, :height 300}, :fill (hslx 300 20 30), :alpha 0.8})
+       (text
+        {:text "Winning!",
+         :position {:x 40, :y 20},
+         :style {:fill (hslx 30 90 100), :font-size 200, :font-family "Josefin Sans"}}))))))
