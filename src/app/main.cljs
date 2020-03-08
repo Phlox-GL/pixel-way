3215715c5a39cb233c2a40075736bb9c70c1ec2e
jiyinyiyong <jiyinyiyong@gmail.com>
2020-03-09 01:07:29 +0800

    preload web fonts

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -5,7 +5,8 @@
             [app.container :refer [comp-container]]
             [app.schema :as schema]
             ["shortid" :as shortid]
-            [app.updater :refer [updater]]))
+            [app.updater :refer [updater]]
+            ["fontfaceobserver" :as FontFaceObserver]))
 
 (defonce *store (atom schema/store))
 
@@ -14,13 +15,17 @@
   (let [op-id (shortid/generate), op-time (.now js/Date)]
     (reset! *store (updater @*store op op-data op-id op-time))))
 
+(def global-fonts
+  (js/Promise.all
+   (array (.load (FontFaceObserver. "Josefin Sans")) (.load (FontFaceObserver. "Hind")))))
+
 (defn start-undulating! []
   (dispatch! :undulate nil)
   (js/setTimeout (fn [] (start-undulating!)) (* 6000 (js/Math.pow (js/Math.random) 5))))
 
 (defn main! []
   (comment js/console.log PIXI)
-  (render! (comp-container @*store) dispatch! {})
+  (-> global-fonts (.then (fn [] (render! (comp-container @*store) dispatch! {}))))
   (add-watch *store :change (fn [] (render! (comp-container @*store) dispatch! {})))
   (start-undulating!)
   (println "App Started"))
